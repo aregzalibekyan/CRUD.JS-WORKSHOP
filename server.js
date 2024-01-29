22b364d619f0a5a043394d96be74ae2bb73afcef
aregzalibekyan <areg.zalibekyan@tumo.org>
2024-01-29 19:29:23 +0400

    das 6

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-  const express = require("express");
+const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
@@ -7,14 +7,64 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://aregzalibekyan:613235Ruz.@cluster0.p1fxrej.mongodb.net/sample_mflix";
 const db1 = mongoose.connection;
+const {Schema} = mongoose;
 db1.on("error", console.error.bind(console, "Connection error:"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.get("/", async function (req, res) {
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/form.html"));
+  const connectionString =
+    "mongodb+srv://aregzalibekyan:613235Ruz.@cluster0.p1fxrej.mongodb.net/Tumo_products";
+  mongoose.connect(connectionString, { useUnifiedTopology: true });
+
+  db1.once('open', async () => {
+    try {
+      const schema = new Schema({
+        name: String,
+        info: String,
+        price: Number,
+        img: String
+      })
+      const Product = mongoose.model('Product', schema);
+      schema.path('_id');
+      var a = new Product;
+      a.name = 'aa'
+      a.save()
+
+    } catch (e) {
+
+    } finally {
+      console.log("Connection closed");
+      mongoose.connection.close();
+    }
+  })
+
+
 });
+
+app.get("/list", (req, res) => {
+
+  mongoose.connect(connectionString, { useUnifiedTopology: true });
+  db1.once('open', async () => {
+    try {
+      const theaters = await mongoose.connection.db.collection("theaters").find({
+        'location.address.city': 'Bloomington'
+      }).toArray()
+      res.render('../public/form.ejs', {
+        theaters: theaters
+      });
+
+    } catch (e) {
+
+    } finally {
+      console.log("Connection closed");
+      mongoose.connection.close();
+    }
+  })
+
+})
 app.post("/addInfo", function (req, res) {
   let [surname, age, email] = [req.body.surname, req.body.age, req.body.email];
   mongoose.connect(connectionString, { useUnifiedTopology: true });
@@ -27,7 +77,7 @@ app.post("/addInfo", function (req, res) {
       });
       console.log(
         await mongoose.connection.db.collection("users").findOne({
-          email:email,
+          email: email,
         })
       );
     } catch (e) {
@@ -48,30 +98,3 @@ app.listen(port, () => {
   console.log(`App is running on ${port} port`);
 });
 
-// Replace the connection string with your MongoDB connection string
-
-// Connect to MongoDB
-mongoose.connect(connectionString, { useUnifiedTopology: true });
-
-// Check the connection
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection error:"));
-db.once("open", async () => {
-  try {
-    await mongoose.connection.db
-      .collection("users")
-      .deleteOne({ name: "Gexam" });
-    console.log(
-      await mongoose.connection.db.collection("users").findOne({
-        name: "Gexam",
-      })
-    );
-  } catch (e) {
-    throw new Error(`Something wrong: ${e}`);
-  } finally {
-    console.log("Connection closed");
-    mongoose.connection.close();
-  }
-  // You can add additional code here for testing or other operations
-  // Make sure to close the connection when you're done
-});
